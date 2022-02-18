1a11921af220e9f8ff60e21c23a8abbe0108e098
Vitor Paes <vitor.paes@compasso.com.br>
2022-02-18 02:06:37 -0300

    DONE - Add Person Page and translations

---
+++ src/pages/DetailsPage.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Avatar, Button, Container, Grid, Typography } from '@mui/material'
+import Header from 'src/components/Header/Header'
+import { handleSelect } from 'src/state'
+import { Redirect, useHistory, useParams } from 'react-router-dom'
+import { useSite } from 'src/driver/MultisiteContext'
+import { iTheme, makeStyles } from 'src/helpers/SystemTheme'
+import { FiChevronLeft } from 'react-icons/fi'
+
+interface DetailsPageParams {
+  id: string
+}
+
+const useStyles = makeStyles((theme: iTheme) => ({
+  fontColor: {
+    color: theme.palette.text.primary,
+  },
+  headerGrid: {
+    marginTop: theme.spacing(3),
+  },
+  headerInfo: {
+    padding: theme.spacing(2),
+  },
+  headerInfoDescription: {
+    paddingLeft: theme.spacing(2),
+  },
+}))
+
+const DetailsPage: React.FC = () => {
+  const { id } = useParams<DetailsPageParams>()
+  const { routes } = useSite()
+  const history = useHistory()
+  const classes = useStyles()
+  const person = handleSelect((state) =>
+    state.people.list.find((person) => person.id === Number(id))
+  )
+
+  const redirectToHomePage = () => history.push(routes.HOME)
+
+  if (!person) return <Redirect to={routes.HOME} />
+
+  return (
+    <Container>
+      <Header>
+        <Grid container className={classes.headerGrid}>
+          <Grid item container md={1} alignItems={'center'}>
+            <Button
+              color={'primary'}
+              size={'small'}
+              onClick={redirectToHomePage}
+            >
+              <FiChevronLeft size={80} />
+            </Button>
+          </Grid>
+          <Grid item md={2}>
+            <Avatar
+              src={person.avatar}
+              alt={person.name}
+              sx={{ width: '100%', height: 'auto' }}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <div className={classes.headerInfo}>
+              <Typography variant={'h2'} className={classes.fontColor}>
+                {person.name}
+              </Typography>
+              <div className={classes.headerInfoDescription}>
+                <Typography variant={'body2'} className={classes.fontColor}>
+                  {person.description}
+                </Typography>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Header>
+    </Container>
+  )
+}
+
+export default DetailsPage
